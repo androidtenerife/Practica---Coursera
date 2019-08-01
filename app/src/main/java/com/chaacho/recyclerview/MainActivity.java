5cb07f03281bdad90bac80050ae79dea6ee140c8
Herbert Daniel Rodríguez <tenerife.developer@gmail.com>
2019-08-01 11:40:26 +0100

    Implementacion de metodos Del adaptador. OnBindView Holder. OnCreate ViewHolder Get ItemCount

--- app/src/main/java/com/chaacho/recyclerview/MainActivity.java
+++ app/src/main/java/com/chaacho/recyclerview/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toolbar;
 
@@ -11,28 +12,29 @@ import com.chaacho.recyclerview.pojo.Mascotas;
 
 import java.util.ArrayList;
 
+import static android.widget.LinearLayout.VERTICAL;
+
 
 public class MainActivity extends AppCompatActivity {
     //TODO 1 Agregar un Arraylist de Mascotas
     ArrayList<Mascotas> mascotas;
+    Activity activity;
     private RecyclerView listaMascotas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-        inicializarListaMascotas();
+        //Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        //setSupportActionBar(myToolbar);
 
         ArrayList<String> mascotasContacto = new ArrayList<String>();
-       // ListView lstContactos = (ListView)findViewById(R.id.lvMascotas);
-        //lstContactos.setAdapter(new ArrayAdapter<String>(this,android.R.layout.activity_list_item));
-        listaMascotas = (RecyclerView) findViewById(R.id.cvMascota);
+        listaMascotas = (RecyclerView) findViewById(R.id.rvMascotas);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         llm.setOrientation(RecyclerView.VERTICAL);
-        //listaMascotas.setLayoutManager(llm);
-
+        listaMascotas.setLayoutManager(llm);
+        inicializarListaMascotas();
+        inicializarAdaptador();
 
 
 
@@ -52,9 +54,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void inicializarAdaptador(){
-        MascotaAdaptador adaptador = new MascotaAdaptador(mascotas
-        );
+        MascotaAdaptador adaptador = new MascotaAdaptador(mascotas,activity);
         listaMascotas.setAdapter(adaptador);
+
 
     }
 }
--- app/src/main/java/com/chaacho/recyclerview/MascotaAdaptador.java
+++ app/src/main/java/com/chaacho/recyclerview/MascotaAdaptador.java
@@ -1,10 +1,14 @@
 package com.chaacho.recyclerview;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,15 +19,14 @@ import com.chaacho.recyclerview.pojo.Mascotas;
 
 import java.util.ArrayList;
 
-public class
-MascotaAdaptador extends Adapter<MascotaAdaptador.MascotaViewHolder> {
+public class MascotaAdaptador extends RecyclerView.Adapter<MascotaAdaptador.MascotaViewHolder> {
+    ArrayList<Mascotas>mascotas;
+    Activity activity;
 
-ArrayList<Mascotas>mascotas;
-
-    public MascotaAdaptador(ArrayList<Mascotas> mascotas) {
+    public MascotaAdaptador(ArrayList<Mascotas> mascotas, Activity activity) {
         this.mascotas=mascotas;
+        this.activity=activity;
     }
-
     @NonNull
     @Override
     public MascotaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -35,23 +38,38 @@ ArrayList<Mascotas>mascotas;
 
     @Override
     //Asocia cada elemento de la lista con cada view
-    public void onBindViewHolder(@NonNull MascotaViewHolder mascotaViewHolder, int position) {
-        Mascotas mascota = mascotas.get(position);
+    public void onBindViewHolder(@NonNull final MascotaViewHolder mascotaViewHolder, int position) {
+        final Mascotas mascota = mascotas.get(position);
         mascotaViewHolder.tvTel.setText(mascota.getTelefono());
+        mascotaViewHolder.tvEmail.setText(mascota.getEmail());
+        mascotaViewHolder.tvNombre.setText(mascota.getNombre());
+        mascotaViewHolder.imgFoto.setImageResource(mascota.getFoto());
+        mascotaViewHolder.imgFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(view.getContext(), mascota.getNombre(), Toast.LENGTH_SHORT).show();
+                //Intent intent = new Intent (,Detalle.class);
+                Intent intent1 = new Intent(activity,Detalle.class);
+                activity.startActivity(intent1);
+
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return mascotas.size();
     }
 
     //TODO Agregar la Clase Anidada VIewHolder
     public static class MascotaViewHolder extends RecyclerView.ViewHolder {
         private ImageView imgFoto;
         private TextView tvNombre;
-        private TextView tvApellido;
         private TextView tvTel;
         private TextView tvEmail;
+
 
 
         // Todo Crear el constructor de la superclase
@@ -59,9 +77,8 @@ ArrayList<Mascotas>mascotas;
             super(itemView);
             imgFoto = itemView.findViewById(R.id.ivPerro);
             tvNombre = itemView.findViewById(R.id.tvNombre);
-            tvApellido = itemView.findViewById(R.id.tvApellido);
             tvEmail = itemView.findViewById(R.id.tvMail);
-            tvTel = itemView.findViewById(R.id.tvTelefono);
+            tvTel = itemView.findViewById(R.id.tvTel);
 
 
 
